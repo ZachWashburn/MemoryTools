3f177b83c0700838d57c19e4c56a1e26133f6290
ZachWashburn <91298195+ZachWashburn@users.noreply.github.com>
2021-11-20 14:09:08 -0500

    Add New Functions, Fix XML Parsing
    - Add Stack Pattern Scanning
    
    - Add Stack Creation
    
    - Add MTFree

--- MemoryTools/MemoryTools.cpp
+++ MemoryTools/MemoryTools.cpp
@@ -8,18 +8,42 @@
 #include <Psapi.h>
 #include <intsafe.h>
 #include <malloc.h>
+#include <winternl.h>
+#include <intrin.h>
+
+typedef struct _THREAD_BASIC_INFORMATION
+{
+	NTSTATUS                ExitStatus;
+	PVOID                   TebBaseAddress;
+	CLIENT_ID               ClientId;
+	KAFFINITY               AffinityMask;
+	KPRIORITY               Priority;
+	KPRIORITY               BasePriority;
+} THREAD_BASIC_INFORMATION, * PTHREAD_BASIC_INFORMATION;
 
 #define INRANGE(x,a,b)    (x >= a && x <= b) 
 #define getBits( x )    (INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
 #define getByte( x )    (getBits(x[0]) << 4 | getBits(x[1]))
 
-_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanMemoryRegion(_In_reads_bytes_(nRegionSize) void* pBaseAddress, _In_ size_t nRegionSize, _In_ const char* pszPattern)
+
+void InvalidParameterHandler(const wchar_t* wszExpression,
+	const wchar_t* wszFunction,
+	const wchar_t* wszFile,
+	unsigned int nLine,
+	uintptr_t pReserved)
+{
+	printf("MemoryTools :: Invalid parameters failure!");
+}
+
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanMemoryRegion(_In_reads_bytes_(nRegionSize) void* pBaseAddress, _In_ size_t nRegionSize, _In_z_ const char* pszPattern)
 {
 	const char* pat = pszPattern;
 	BYTE* firstMatch = 0;
 	BYTE* rangeStart = (BYTE*)pBaseAddress;
 	BYTE* rangeEnd = rangeStart + nRegionSize;
-	for (BYTE* pCur = rangeStart; pCur < rangeEnd; pCur++)
+
+	// This may possibly cause issues on huge ranges
+	for (BYTE* pCur = rangeStart;pCur < rangeEnd ; pCur++)
 	{
 		if (!*pat)
 			return firstMatch;
@@ -48,7 +72,54 @@ _Ret_maybenull_ void* MTCALL MemoryTools::PatternScanMemoryRegion(_In_reads_byte
 	return (void*)nullptr;
 }
 
-_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanModuleHandle(_In_ void* hModule, _In_ const char* pszPattern)
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanMemoryRegionReverse(_In_reads_bytes_(nRegionSize) void* pBaseAddress, _In_ size_t nRegionSize, _In_z_ const char* pszPattern)
+{
+	const char* pat = pszPattern;
+	BYTE* firstMatch = 0;
+	BYTE* rangeStart = (BYTE*)pBaseAddress;
+	BYTE* rangeEnd = rangeStart - nRegionSize;
+
+	char* pReversedString = MemoryTools::GetPatternReversed(pszPattern);
+
+	if (!pReversedString)
+		return nullptr;
+
+	pszPattern = pReversedString;
+	// This may possibly cause issues on huge ranges
+	for (BYTE* pCur = rangeStart; pCur > rangeEnd; pCur--)
+	{
+		if (!*pat)
+			return firstMatch;
+
+		if (*(PBYTE)pat == '\?' || *(BYTE*)pCur == getByte(pat))
+		{
+			if (!firstMatch)
+				firstMatch = pCur;
+
+			if (!pat[2])
+			{
+				free((void*)pszPattern);
+				return firstMatch;
+			}
+
+			if (*(PWORD)pat == '\?\?' || *(PBYTE)pat != '\?')
+				pat += 3;
+
+			else
+				pat += 2;    //one ?
+		}
+		else
+		{
+			pat = pszPattern;
+			firstMatch = 0;
+		}
+	}
+
+	free((void*)pszPattern);
+	return (void*)nullptr;
+}
+
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanModuleHandle(_In_ void* hModule, _In_z_ const char* pszPattern)
 {
 	MODULEINFO modInfo;
 	
@@ -58,7 +129,7 @@ _Ret_maybenull_ void* MTCALL MemoryTools::PatternScanModuleHandle(_In_ void* hMo
 	return (void*)nullptr;
 }
 
-_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanModule(_In_ const char* pszModuleName, _In_ const char* pszPattern)
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanModule(_In_z_ const char* pszModuleName, _In_z_ const char* pszPattern)
 {
 	HMODULE hModule;
 	hModule = GetModuleHandleA(pszModuleName);
@@ -69,7 +140,7 @@ _Ret_maybenull_ void* MTCALL MemoryTools::PatternScanModule(_In_ const char* psz
 	return PatternScanModuleHandle(hModule, pszPattern);
 }
 
-_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanCurrentProcess(_In_ const char* pszPattern)
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanCurrentProcess(_In_z_ const char* pszPattern)
 {
 	HMODULE hMods[1024];
 	DWORD cbNeeded;
@@ -90,7 +161,7 @@ _Ret_maybenull_ void* MTCALL MemoryTools::PatternScanCurrentProcess(_In_ const c
 }
 
 
-_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanHeap(_In_/*PROCESS_HEAP_ENTRY*/ void* pHeapEntry, _In_ const char* pszPattern)
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanHeap(_In_/*PROCESS_HEAP_ENTRY*/ void* pHeapEntry, _In_z_ const char* pszPattern)
 {
 	MEMORY_BASIC_INFORMATION MemInfo;
 	DWORD dwOldProtect = 0;
@@ -114,7 +185,7 @@ _Ret_maybenull_ void* MTCALL MemoryTools::PatternScanHeap(_In_/*PROCESS_HEAP_ENT
 }
 
 
-_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanCurrentProcessHeaps(_In_ const char* pszPattern)
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanCurrentProcessHeaps(_In_z_ const char* pszPattern)
 {
 	DWORD dwNumberOfHeaps;
 	DWORD HeapsIndex;
@@ -178,32 +249,69 @@ _Ret_maybenull_ void* MTCALL MemoryTools::PatternScanCurrentProcessHeaps(_In_ co
 }
 
 
+_Ret_maybenull_ void* MTCALL MemoryTools::PatternScanStack(_In_ void* pThreadHandle, _In_z_ const char* pszPattern)
+{
+	THREAD_BASIC_INFORMATION tbiInfo;
+	NT_TIB* pTib;
+
+
+	if (NtQueryInformationThread((HANDLE)pThreadHandle, (THREADINFOCLASS)NULL/*ThreadBasicInformation*/, &tbiInfo, sizeof(THREAD_BASIC_INFORMATION), NULL) != S_OK)
+		return nullptr;
+
+
+	// Read TIB
+	pTib = (NT_TIB*)tbiInfo.TebBaseAddress;
+	return MemoryTools::PatternScanMemoryRegion((char*)pTib->StackLimit, (char*)pTib->StackBase - (char*)pTib->StackLimit, pszPattern);
+}
+
+_Ret_maybenull_ void* MTCALL  MemoryTools::PatternScanCurrentStack(_In_z_ const char* pszPattern)
+{
+	return MemoryTools::PatternScanStack(GetCurrentThread(), pszPattern);
+}
 
 _Success_(return != false) bool MTCALL MemoryTools::PlaceJumpRel32x86(_Out_writes_bytes_all_(5) void* pWriteAddress, _In_ void* pJumpAddress)
 {
+	if (!pWriteAddress || !pJumpAddress)
+		return false;
+
+
 	DWORD dwOldProtect;
+	char* pRelativeDifference = nullptr;
+
 	if (!VirtualProtect(pWriteAddress, 5, PAGE_EXECUTE_READWRITE, &dwOldProtect))
 		return false;
+
 	memset(pWriteAddress, 0x90, 5);
-	char* pRelativeDifference = (char*)(((char*)pJumpAddress - (char*)pWriteAddress) - 5);
+	pRelativeDifference = (char*)(((char*)pJumpAddress - (char*)pWriteAddress) - 5);
 	*(unsigned char*)(pWriteAddress) = 0xE9;
 	*(void**)((char*)pWriteAddress + sizeof(unsigned char*)) = pRelativeDifference;
+
 	if (!VirtualProtect(pWriteAddress, 5, dwOldProtect, &dwOldProtect))
 		return false;
+
 	return true;
 }
 
 _Success_(return != false) bool MTCALL MemoryTools::PlaceCallRel32x86(_Out_writes_bytes_all_(5) void* pWriteAddress, _In_ void* pJumpAddress)
 {
+	if (!pWriteAddress || !pJumpAddress)
+		return false;
+
 	DWORD dwOldProtect;
+	char* pRelativeDifference = nullptr;
+
 	if (!VirtualProtect(pWriteAddress, 5, PAGE_EXECUTE_READWRITE, &dwOldProtect))
 		return false;
+
 	memset(pWriteAddress, 0x90, 5);
-	char* pRelativeDifference = (char*)(((char*)pJumpAddress - (char*)pWriteAddress) - 5);
+	pRelativeDifference = (char*)(((char*)pJumpAddress - (char*)pWriteAddress) - 5);
 	*(unsigned char*)(pWriteAddress) = 0xE8;
 	*(void**)((char*)pWriteAddress + sizeof(unsigned char*)) = pRelativeDifference;
+
+
 	if (!VirtualProtect(pWriteAddress, 5, dwOldProtect, &dwOldProtect))
 		return false;
+
 	return true;
 }
 
@@ -269,4 +377,122 @@ _Ret_maybenull_ void* MTCALL MemoryTools::RelativeToAbsolute(_In_reads_(sizeof(v
 
 	// Yes the casts are ugly
 	return (void*)((int)((char*)ptr + sizeof(void*)) + *(char**)ptr);
+}
+
+_Ret_maybenull_
+_Null_terminated_
+_Must_inspect_result_
+char* MTCALL MemoryTools::GetPatternReversed(_In_opt_z_ const char* szPattern)
+{
+	if (!szPattern)
+		return nullptr;
+
+	size_t nSize = 0;
+	char* pszReversedString = nullptr;
+
+	// Use SEH incase szPattern is massive!
+	_invalid_parameter_handler pOldHandler = _get_invalid_parameter_handler();
+	_set_invalid_parameter_handler(&InvalidParameterHandler);
+	nSize = strnlen_s(szPattern, 8192);
+	_set_invalid_parameter_handler(pOldHandler);
+
+	if (!nSize)
+		return nullptr;
+	
+
+	pszReversedString = (char*)calloc(nSize, sizeof(char));
+	
+	if (!pszReversedString)
+		return nullptr;
+
+	int j = nSize - 1;
+	for (unsigned int i = 0; i < nSize - 1; i++, j--)
+		pszReversedString[i] = j;
+
+	return pszReversedString;
+}
+
+void MTCALL MemoryTools::MTFree(_In_opt_ void* ptr)
+{
+	if(ptr)
+		free(ptr);
+}
+
+
+void MTCALL MemoryTools::CreateNewStackx86(_In_ size_t nMinStackSize, _In_opt_ bool bExecutable, _In_opt_ MemoryTools::StackStore_t* pStackStore)
+{
+
+	NT_TIB* pTIB;
+	DWORD dwOldProtect;
+	size_t nNewStackSize;
+	size_t nDataSize;
+	void* pStack;
+	void* pReturnAddress;
+	void* pOldStack;
+
+	_asm { 	// Get our current TIB
+		mov eax, gs:00
+		mov pTIB, eax
+	}
+
+	if (!pTIB)
+		return;
+	pOldStack = pTIB->StackLimit;
+	nDataSize = (char*)pTIB->StackBase - (char*)pTIB->StackLimit;
+	nNewStackSize = max(nDataSize, nMinStackSize);
+	pStack = malloc(nNewStackSize);
+
+	if (!pStack)
+		return;
+
+	memset(pStack, 0x00, nNewStackSize);
+
+	pReturnAddress = _ReturnAddress();
+
+	VirtualProtect((char*)pTIB->StackLimit, nDataSize, PAGE_READWRITE, &dwOldProtect);
+
+	if (bExecutable) {
+		if (!VirtualProtect(pStack, nNewStackSize, PAGE_EXECUTE_READWRITE, &dwOldProtect))
+		{
+			free(pStack);
+			return;
+		}
+	}
+
+	memcpy(pStack, (char*)pTIB->StackLimit, nDataSize);
+
+	if (pStackStore)
+	{
+		pStackStore->m_pStack = pTIB->StackLimit;
+		pStackStore->m_nStackSize = nDataSize;
+	}
+
+	pTIB->StackLimit = pStack;
+	pTIB->StackBase = (char*)pStack + nNewStackSize;
+
+	*((char**)&pStack) += nNewStackSize;
+
+	_asm {
+		mov edx, pOldStack
+		sub edx, esp
+		mov eax, pStack
+		add eax, edx
+		mov pStack, eax
+
+		mov ecx, ebp
+		sub ecx, esp
+		mov eax, pStack
+		mov ebx, pReturnAddress
+		mov esp, eax
+		add ecx, esp
+		mov ebp, ecx
+
+		push ebx
+		ret
+	}
+}
+
+void MTCALL MemoryTools::RestoreStackx86(_In_ MemoryTools::StackStore_t* pStackStore)
+{
+
 }
--- MemoryTools/MemoryTools.h
+++ MemoryTools/MemoryTools.h
@@ -8,46 +8,69 @@
 #define MEMTOOLS_STATIC_LIBRARY
 #ifndef MEMTOOLS_STATIC_LIBRARY
 #define DLLEXPORT __declspec(dllexport)
-#define EXTERNC extern "C" {
-#define EXTERNCCLOSE };
 #else 
 #define DLLEXPORT 
-#define EXTERNCOPEN /**/
-#define EXTERNCCLOSE /**/
 #endif
 
+#define EXTERNCOPEN extern "C" {
+#define EXTERNCCLOSE };
 // Using stdcall for easier use in x86 (not needing to clean up the stack)
 #define MTCALL __stdcall
 
 namespace MemoryTools
 {
-	EXTERNCOPEN
-		
 
+
+	/* Definitions */
+
+	struct StackStore_t
+	{
+		size_t m_nStackSize = 0;
+		_Field_size_bytes_(m_nStackSize)
+		void* m_pStack = nullptr;
+	};
+
+
+	/* Functions */
+
+	EXTERNCOPEN
 		/// <c>PatternScanMemoryRegion</c> 
-		/// <summary> Find a Pattern within <paramref name="pBaseAddress"/> to <paramref name="pBaseAddress"/> + <paramref name="nRegionSize"/>.</summary>
+		/// <summary> Find a Pattern within pBaseAddress to pBaseAddress + nRegionSize.</summary>
 		/// <param name="pBaseAddress"> Base Address To Scan From.</param>
 		/// <param name="nRegionSize"> Amount of bytes to scan from pBaseAddress.</param>
 		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format.</param>
 		/// <returns>  Address Pattern Found At Or NULL (no match).</returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL PatternScanMemoryRegion(
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanMemoryRegion
+		(
 			_In_reads_bytes_(nRegionSize) void* pBaseAddress,
-			_In_ size_t nRegionSize, 
-			_In_ const char* pszPattern
+			_In_ size_t nRegionSize,
+			_In_z_ const char* pszPattern
 		);
+
+
+		/// <c>PatternScanMemoryRegionReverse</c> 
+		/// <summary> Find a Pattern within pBaseAddress to pBaseAddress + nRegionSize going in reverse order.</summary>
+		/// <param name="pBaseAddress"> Base Address To Scan From.</param>
+		/// <param name="nRegionSize"> Amount of bytes to scan from pBaseAddress.</param>
+		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format, function automatically reversed code bytes </param>
+		/// <returns>  Address Pattern Found At Or NULL to signify no match .</returns>
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanMemoryRegionReverse
+		(
+			_In_reads_bytes_(nRegionSize) void* pBaseAddress,
+			_In_ size_t nRegionSize,
+			_In_z_ const char* pszPattern
+		);
+
 
 		/// <c>PatternScanModule</c> 
 		/// <summary> Finds a Loaded Module, and Scans its Memory For a Pattern.</summary> 
 		/// <param name="pszModuleName"> String With Name Of The Module To Scan.</param>
 		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format.</param>
 		/// <returns> <strong> Address Pattern Found At Or NULL (no match / module not found) </strong>.</returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL PatternScanModule(
-			_In_ const char* pszModuleName, 
-			_In_ const char* pszPattern
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanModule
+		(
+			_In_z_ const char* pszModuleName,
+			_In_z_ const char* pszPattern
 		);
 
 
@@ -56,34 +79,31 @@ namespace MemoryTools
 		/// <param name="hModule"> The Handle To The Module Found </param>
 		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format </param>
 		/// <returns> <strong> Address Pattern Found At Or NULL (no match / module info not acquired) </strong> </returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL PatternScanModuleHandle(
-			_In_ void* hModule, 
-			_In_ const char* pszPattern
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanModuleHandle
+		(
+			_In_ void* hModule,
+			_In_z_ const char* pszPattern
 		);
 
 		/// <c> PatternScanCurrentProcess </c> 
 		/// <summary> Scans All Process Modules For a Pattern </summary>
 		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format </param>
 		/// <returns> <strong> Address Pattern Found At Or NULL (no match) </strong> </returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL PatternScanCurrentProcess(
-			_In_ const char* pszPattern
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanCurrentProcess
+		(
+			_In_z_ const char* pszPattern
 		);
 
-	
+
 		/// <c> PatternScanHeap </c> 
 		/// <summary> Scans a Specific Memory Heap For a Pattern </summary>
 		/// <param name="pHeapEntry"> A Pointer to a PROCESS_HEAP_ENTRY structure </param>
 		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format </param>
 		/// <returns> <strong> Address Pattern Found At Or NULL (no match) </strong> </returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL PatternScanHeap(
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanHeap
+		(
 			_In_ void* pHeapEntry, /* PROCESS_HEAP_ENTRY* */
-			_In_ const char* pszPattern
+			_In_z_ const char* pszPattern
 		);
 
 
@@ -91,22 +111,41 @@ namespace MemoryTools
 		/// <summary> Scans All Heaps For The Current Process For a Pattern </summary>
 		/// <param name="pszPattern"> The String To Scan For in  XX XX ? XX XX  format </param>
 		/// <returns> <strong> Address Pattern Found At Or NULL (no match) </strong> </returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL PatternScanCurrentProcessHeaps(
-			_In_ const char* pszPattern
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanCurrentProcessHeaps
+		(
+			_In_z_ const char* pszPattern
 		);
+
+		/// <c> PatternScanStack </c> 
+		/// <summary> Scans The Stack Top To Bottom And Returns First Match </summary>
+		/// <param name="pThreadHandle"> A Handle To The Thread That We Should Scan The Stack For </param>
+		/// <param name="pszPattern"> The String To Scan For in XX XX ? XX XX  format </param>
+		/// <returns> <strong> Address Pattern Found At Or NULL (no match) </strong> </returns>
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanStack
+		(
+			_In_ void* pThreadHandle,
+			_In_z_ const char* pszPattern
+		);
+
+		/// <c> PatternScanStack </c> 
+		/// <summary> Scans The Stack Top To Bottom And Returns First Match </summary>
+		/// <param name="pszPattern"> The String To Scan For in XX XX ? XX XX  format </param>
+		/// <returns> <strong> Address Pattern Found At Or NULL (no match) </strong> </returns>
+		DLLEXPORT _Ret_maybenull_ void* MTCALL PatternScanCurrentStack
+		(
+			_In_z_ const char* pszPattern
+		);
+
 
 		/// <c> PlaceJumpRel32x86 </c> 
 		/// <summary> Places a Relative Jump 32bit to pJumpAddress.</summary>
 		/// <param name="pWriteAddress"> A Pointer To The Address To Place The Jump.</param>
 		/// <param name="pJumpAddress"> A Pointer To The Address The Jump Points To.</param>
 		/// <returns> <strong> true if the function succeeds, failure is caused do to a VirtualProtect failure.</strong> </returns>
-		DLLEXPORT
-		_Success_(return != false)
-		bool MTCALL PlaceJumpRel32x86(
-				_Out_writes_bytes_all_(5) void* pWriteAddress,
-				_In_ void* pJumpAddress
+		DLLEXPORT _Success_(return != false) bool MTCALL PlaceJumpRel32x86
+		(
+			_Out_writes_bytes_all_(5) void* pWriteAddress,
+			_In_ void* pJumpAddress
 		);
 
 
@@ -115,11 +154,10 @@ namespace MemoryTools
 		/// <param name="pWriteAddress"> A Pointer To The Address To Place The Call </param>
 		/// <param name="pCallAddress"> A Pointer To The Address The Call Points To </param>
 		/// <returns> <strong> true if the function succeeds, failure is caused do to a VirtualProtect failure </strong> </returns>
-		DLLEXPORT
-		_Success_(return != false)
-		bool MTCALL PlaceCallRel32x86(
-				_Out_writes_bytes_all_(5) void* pWriteAddress,
-				_In_ void* pCallAddress
+		DLLEXPORT _Success_(return != false) bool MTCALL PlaceCallRel32x86
+		(
+			_Out_writes_bytes_all_(5) void* pWriteAddress,
+			_In_ void* pCallAddress
 		);
 
 		/// <c> WriteNOPs </c>
@@ -127,13 +165,33 @@ namespace MemoryTools
 		/// <param name="pWriteAddress">  A Pointer To The Address To Place The NOP OpCodes </param>
 		/// <param name="nDataSize"> Amount of Bytes to Overwrite </param>
 		/// <returns> <strong> true if the function succeeds, failure is caused do to a VirtualProtect failure </strong> </returns>
-		DLLEXPORT
-		_Success_(return != false)
-		bool MTCALL WriteNOPs(
-				_Out_writes_bytes_all_(nDataSize) void* pWriteAddress,
-				_In_ size_t nDataSize
+		DLLEXPORT _Success_(return != false) bool MTCALL WriteNOPs
+		(
+			_Out_writes_bytes_all_(nDataSize) void* pWriteAddress,
+			_In_ size_t nDataSize
 		);
 
+
+		/// <c> CreateNewExecutableStackx86 </c>
+		/// <summary> Create A New Stack, StackStore_t must be static/thread_local/global var! </summary>
+		/// <param name="nMinStackSize"> Minimum Size The New Stack Can Be </param>
+		/// <param name="bExecutable"> Where to Mark The Page Protection As Executable </param>
+		/// <param name="pStackStore"> A Pointer to a StackStore_t Object, I recommend a thread_local var (can't be on stack) </param>
+		DLLEXPORT void MTCALL CreateNewStackx86
+		(
+			_In_ size_t nMinStackSize,
+			_In_opt_ bool bExecutable,
+			_In_opt_ StackStore_t * pStackStore
+		);
+		
+
+		/// <c> RestoreStackx86 </c>
+		/// <summary> Restore Stack From StackStore Obj </summary>
+		/// <param name="pStackStore"> A Pointer to a StackStore_t Object </param>
+		DLLEXPORT void MTCALL RestoreStackx86
+		(
+			_In_ StackStore_t * pStackStore
+		);
 
 		/// <c> DoesMemoryHaveAttributes </c> 
 		/// <summary> Checks Memory Region for page characteristics *pnMatchable amount must be 0 pass, 0xFFFFFFFF for any characteristic fields you don't care for	</summary>
@@ -144,8 +202,8 @@ namespace MemoryTools
 		/// <param name="PageType"> The Page Type To Check For (i.e.) MEM_IMAGE, Pass 0xFFFFFFF if it doesn't matter </param>
 		/// <param name="pnMatchableAmount"> An Optional Pointer that returns how many bytes met these requirements </param>
 		/// <returns> <strong> True if all bytes checked meet the requirements passed </strong> </returns>
-		DLLEXPORT
-		bool MTCALL DoesMemoryHaveAttributes(
+		DLLEXPORT bool MTCALL DoesMemoryHaveAttributes
+		(
 			_In_ void* ptr,
 			_In_ size_t nDataSize,
 			_In_ int PageState,
@@ -161,8 +219,8 @@ namespace MemoryTools
 		/// <param name="nDataSize"> Amount Of Bytes to Check </param>
 		/// <param name="pnReadableAmount"> An Optional Pointer that returns how many bytes were readable </param>
 		/// <returns> <strong> True if all bytes checked were readable </strong> </returns>
-		DLLEXPORT
-		bool MTCALL IsMemoryRangeReadable(
+		DLLEXPORT bool MTCALL IsMemoryRangeReadable
+		(
 			_In_ void* ptr,
 			_In_ size_t nDataSize,
 			_Inout_opt_ size_t* pnReadableAmount = nullptr
@@ -172,17 +230,42 @@ namespace MemoryTools
 		/// <summary> Converts a Relative Address In Memory To An Absolute One </summary>
 		/// <param name="ptr"> A Pointer To The Address Location In Memory </param>
 		/// <returns> <strong> Absolute Value Of Address, NULL if Memory Is Not Readable </strong> </returns>
-		DLLEXPORT
-		_Ret_maybenull_
-		void* MTCALL RelativeToAbsolute(
+		DLLEXPORT _Ret_maybenull_ void* MTCALL RelativeToAbsolute
+		(
 			_In_reads_(sizeof(void*)) void** ptr
 		);
 
+
+
+
+		/// <c> GetPatternReversed </c> 
+		/// <summary> Reverses a Code Pattern or any string less than 8192 </summary>
+		/// <param name="szPattern"> A Pointer To The String </param>
+		/// <returns> <strong> Returns a allocated new string must free use MTFree </strong> </returns>
+		DLLEXPORT _Ret_maybenull_ _Null_terminated_ _Must_inspect_result_ char* MTCALL GetPatternReversed
+		(
+			_In_opt_z_ const char* szPattern
+		);
+
+		/// <c> MTFree </c> 
+		/// <summary> Frees memory, use incase of custom allocator </summary>
+		/// <param name="ptr"> A Pointer To The Allocated String </param>
+		/// <returns> <strong> void (no return) </strong> </returns>
+		DLLEXPORT void MTCALL MTFree
+		(
+			_In_opt_ void* ptr
+		);
+
 	EXTERNCCLOSE
-}	
+
+
+
+}
 
 namespace MemoryToolsEx {
 
 }
+
 #undef DLLEXPORT
 #endif
+
